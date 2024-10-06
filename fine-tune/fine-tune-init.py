1cd46d3cabf2c245015a2548f12daf98ba7bb7e7
IO32NQ <113096079+IO32NQ@users.noreply.github.com>
2024-10-07 01:11:33 +0200

    added convert db to jsonl

--- fine-tune/fine-tune-init.py
+++ fine-tune/fine-tune-init.py
@@ -1,34 +1,58 @@
 from openai import OpenAI
 import time
 import json
+import sqlite3
 
 with open('secrets.json', 'r') as file:
     secrets = json.load(file)
 
 client = OpenAI(
+    organization = secrets["organization"],
+    project = secrets["project"],
     api_key = secrets["api_key"]
 )
 
-training_file_name = "fine-tune/finetune_dataset_demo.jsonl"
+# Name of the training file
+training_file_name = "finetune_dataset_demo"
 
-#Uploading training file
+# Convert db to jsonl
+try:
+    db = sqlite3.connect("fine-tune/demo_database1.db")
+    cur = db.cursor()
+
+    cur.execute("SELECT * from demo")
+    rows = cur.fetchall()
+
+    data = []
+    for row in rows:
+        data.append({"messages":[{"role": "system", "content": row[0]},
+                                 {"role": "user", "content": row[1]},
+                                 {"role": "assistant", "content": row[2]}]})
+
+    with open("fine-tune/"+training_file_name+".jsonl", "w") as file:
+        for data_line in data:
+            json.dump(data_line, file)
+            file.write("\n")
+except Exception as e:
+    print("An error has occured: ", e)
+    
+
+# Uploading training file
 training_data = client.files.create(
-    file=open(training_file_name, "rb"),
+    file=open("fine-tune/"+training_file_name+".jsonl", "rb"),
     purpose="fine-tune"
 )
-print(f"Training ID: {training_data.id} \n")
+print(f"Training ID: {training_data.id}")
 
-#Create a fine-tuned model
+# Create a fine-tuned model
 response = client.fine_tuning.jobs.create(
   training_file=training_data.id, 
   model="gpt-4o-mini-2024-07-18"
 )
-print(f"Fine-tunning model ID: {response.id} \nFine-tunning model Status: {response.status}")
+print(f"Fine-tunning model ID: {response.id} \nFine-tunning model Status: {response.status} \n")
 
-while response.status != "succeeded" or response.status != "failed":
-    time.sleep(2)
-    print(f"Status: {response.status}")
-
-
-#res = client.fine_tuning.jobs.retrieve("ftjob-ytw7Mrzf5MNUvRPbqVmcnq1o")
-#print(res)
+status = client.fine_tuning.jobs.retrieve(response.id).status
+while status != "succeeded" or status != "failed":
+    time.sleep(5)
+    status = client.fine_tuning.jobs.retrieve(response.id).status
+    print(f"Status: {status}")
